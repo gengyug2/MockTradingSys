3a60a8eda47a4a391d985c7ffa1eed3e181ae7ab
Johnny MOON <gengyug2@gmail.com>
2018-01-01 23:34:26 -0600

    Update bgt_behavior_uniform_price.m

--- bgt_behavior_uniform_price.m
+++ bgt_behavior_uniform_price.m
@@ -1,4 +1,7 @@
 % These scripts are supposed to generate random order
+
+% by Johnny MOON
+
 buy_sell_robot_j=round(rand(1))*2-1;
 price_robot_j=randi(max_price);
 quantity_robot_j=randi(max_quantity);
